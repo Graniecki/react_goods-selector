471fb41f0e7bd03e8fa078f21b99fa3722c72c35
Ihor Hranetskyi <graniecki20@gmail.com>
2020-09-19 13:11:18 +0300

    Updates

---
+++ precache-manifest.b3c356eb1fe2cf8eef53a8fc7cdd3a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614f098c541da4466289965161ef833f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a211f3b61e7f6886359",
+    "url": "./static/css/main.2d138132.chunk.css"
+  },
+  {
+    "revision": "4dd0f18127de32a9553a",
+    "url": "./static/js/2.22b5d565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.22b5d565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a211f3b61e7f6886359",
+    "url": "./static/js/main.d21afbdb.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
